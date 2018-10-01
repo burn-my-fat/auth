1fa8695ddcc9a8985747755bc859b60f8bc4dea0
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-10-01 22:33:32 +0300

    Add Firebase settings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBtjRrKMTrc5nVHCLyuxb_K755oVCeRYYU',
+      authDomain: 'burn-my-fat.firebaseapp.com',
+      databaseURL: 'https://burn-my-fat.firebaseio.com',
+      projectId: 'burn-my-fat',
+      storageBucket: 'burn-my-fat.appspot.com',
+      messagingSenderId: '983341824895'
+    });
+  }
+
   render() {
     return (
       <View>
